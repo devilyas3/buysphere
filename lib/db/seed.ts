332947cdbf48d893ce5423053da8b2713d58acd8
MrPorsche <ilyas.sayyed@hotmail.com>
2025-03-17 20:23:38 +0530

    Implemented Home_Page_Carousel and connected to the MongoDB with Data seed successfully.

---
+++ lib/db/seed.ts
@@ -0,0 +1,28 @@
+import { loadEnvConfig } from "@next/env";
+import { cwd } from "process";
+import data from "../data";
+import { connectToDatabase } from ".";
+import Product from "./models/product.model";
+
+loadEnvConfig(cwd());
+
+const main = async () => {
+    try {
+        const {products} = data;
+        await connectToDatabase(process.env.MONGODB_URI);
+
+        await Product.deleteMany()
+        const createdProducts = await Product.insertMany(products);
+
+        console.log({
+            createdProducts,
+            message: "Successfully seeded Database",
+        });
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to seed Database");
+    }
+};
+
+main();
--- lib/utils.ts
+++ lib/utils.ts
@@ -3,4 +3,20 @@ import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
+
+export const formatNumberWithDecimal = (number: number): string => {
+  const [int, decimal] = number.toString().split('.');
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int;
+};
+
+// PROMPT:[ChatGPT] create toSlung ts arrow function that convert text to lowercase, remove non-word,
+//  non-whitespace, non-hypehen chars, replace whitespace, trim leading hyphens and trim trailinghyphens.
+
+export const toSlug = (text: string): string =>
+  text
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
